5607fbd287fb8392a46dd15c5868f191a64f0825
shinya ishikawa <shinya@shinyanoMacBook-Air.local>
2024-03-07 13:06:24 +0900

    create ui component

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,111 @@
-import Image from 'next/image'
+"use client"
+import { Transition } from '@headlessui/react'
+import { Fragment, useState } from 'react'
+import { useTimeoutFn } from 'react-use'
+import clickCount from './ui/clickCount' 
+import inputUserName from './ui/inputUserName'
 
 export default function Home() {
+  let [isShowing, setIsShowing] = useState(true)
+  let [, , resetIsShowing] = useTimeoutFn(() => setIsShowing(true), 500)
+  const [count,setCount] =useState(0)
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <div className="flex p-6 bg-sky-600">
+        <div className='flex-none w-32'>
+          <img className="h-12 w-32" src="/next.svg" alt="ChitChat Logo"></img>
+        </div>
+        <div className='shrink-0 w-32'>
+          2
+        </div>
+        <div className='flex-none w-32'>
+          3
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='bg-white text-black'>
+        <p>
+        Today I spent most of the day researching ways to take advantage of the fact that bottles can be returned for 10 cents in Michigan, but only 5 cents here.
+        <span className='inline-flex items-baseline'>
+          <img src="/next.svg" alt="ChitChat Logo" className='self-center w-5 rounded-full mx-1'></img>
+          <span>Krmar </span>
+        </span>
+        keeps telling me there is no way to make it work, that he has run the numbers on every possible approach, but I just have to believe there's a way to make it work, there's simply too much opportunity here.
+        </p>
+      </div>
+      <div className='flex mx-auto'>
+        <img className="container mx-auto bg-white flex-center h-12 w-32" src="/next.svg" alt="ChitChat Logo"></img>
+        <img className="container mx-auto bg-sky-600 flex-center h-12 w-32" src="/next.svg" alt="ChitChat Logo"></img>
+        <img className="container mx-auto bg-orange-400 flex-center h-12 w-32" src="/next.svg" alt="ChitChat Logo"></img>
+        <img className="container mx-auto bg-red-400 flex-center h-12 w-32" src="/next.svg" alt="ChitChat Logo"></img>
+        <img className="container mx-auto bg-orange-400 flex-center h-12 w-32" src="/next.svg" alt="ChitChat Logo"></img>
+        <img className="container mx-auto bg-orange-400 flex-center h-12 w-32" src="/next.svg" alt="ChitChat Logo"></img>
+        <img className="container mx-auto bg-orange-400 flex-center h-12 w-32" src="/next.svg" alt="ChitChat Logo"></img>
+        <img className="container mx-auto bg-orange-400 flex-center h-12 w-32" src="/next.svg" alt="ChitChat Logo"></img>
+      </div>
+      <div className='ltr'>
+        <div>１年</div><div className='ms-8 me-8'>石川</div><div>しんや</div>
+      </div>
+      <div className=''>
+          <img className='block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0' src='/picture' alt='my face'></img>
+      </div>
+      <div className="flex flex-col items-center py-16">
+      <div className="h-32 w-32">
+        <Transition
+          as={Fragment}
+          show={isShowing}
+          enter="transform transition duration-[400ms]"
+          enterFrom="opacity-0 rotate-[-120deg] scale-50"
+          enterTo="opacity-100 rotate-0 scale-100"
+          leave="transform duration-200 transition ease-in-out"
+          leaveFrom="opacity-100 rotate-0 scale-100 "
+          leaveTo="opacity-0 scale-95 "
+        >
+          <div className="h-full w-full rounded-md bg-white shadow-lg" />
+        </Transition>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <button
+        onClick={() => {
+          setIsShowing(false)
+          resetIsShowing()
+        }}
+        className="backface-visibility-hidden mt-8 flex transform items-center rounded-full bg-black/20 px-3 py-2 text-sm font-medium text-white transition hover:scale-105 hover:bg-black/30 focus:outline-none active:bg-black/40"
+      >
+        <svg viewBox="0 0 20 20" fill="none" className="h-5 w-5 opacity-70">
+          <path
+            d="M14.9497 14.9498C12.2161 17.6835 7.78392 17.6835 5.05025 14.9498C2.31658 12.2162 2.31658 7.784 5.05025 5.05033C7.78392 2.31666 12.2161 2.31666 14.9497 5.05033C15.5333 5.63385 15.9922 6.29475 16.3266 7M16.9497 2L17 7H16.3266M12 7L16.3266 7"
+            stroke="currentColor"
+            strokeWidth="1.5"
+          />
+        </svg>
+        <span className="ml-3">Click to transition</span>
+      </button>
+      {/*  */}
+      {/* クリックカウント */}
+      <div>
+        {clickCount({})}
       </div>
-    </main>
+      {/* 名前入力フォーム */}
+      {/* 入力できる */}
+      {/* 変化があるたびに変更の値をstateに加える */}
+      <div>
+        {inputUserName({})}
+      </div>
+
+      {/* ・入力フォームで数字以外の文字を入力された瞬間、警告文が出る。
+      ＊どんな文字列でも入る。
+      ・文字数カウントを横に表示する。
+      ・20文字を超えると確認ボタンを押すと警告文が出る*/}
+      {/* 入力フォーム */}
+      {/* 決定ボタン */}
+      {/* 文字数カウント */}
+      {/* 警告文 */}
+      
+     
+
+
+    </div>
+    </div>
   )
 }
---
+++ app/ui/sendMoney.tsx
@@ -0,0 +1,8 @@
+
+export default function SendMoney(props: {}) {
+    return(
+        <div>
+
+        </div>
+    )
+}
