cccbf7fbde61bc195c3f5d62549767b06d665cc9
IshikawaShinya <l.j.6.v.v.c@gmail.com>
2024-03-22 13:28:11 +0900

    backend server 追加

---
+++ pages/api/server.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from fastapi.responses import RedirectResponse
+app = FastAPI()
+
+
+class User(BaseModel):
+    mail: str
+# CORSミドルウェアの設定
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 全てのオリジンを許可
+    allow_credentials=True,
+    allow_methods=["*"],  # 全てのHTTPメソッドを許可
+    allow_headers=["*"],  # 全てのHTTPヘッダーを許可
+)
+
+@app.get("/")
+async def root():
+    return RedirectResponse(url="http://localhost:8000/new")
+
+@app.get("/new")
+async def redirect():
+    return {"message":"OK"}
+
+@app.post("/")
+async def mailreceive(mail:User):
+    print(mail.mail)
+    if(mail.mail == ""):
+        return {"res":"empty"} 
+    elif(mail.mail == "a"):
+        return {"res":"success"}
+    elif(mail != "ishikawa"):
+        return {"res":"wrong mail"}
+    # TODO:status code
+    # return {"res":"status code OK"}
+    
